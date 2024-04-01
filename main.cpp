87d6836ab8e4bcb15a12d4146c54e44314a7fb68
tmanso9 <tmanso92@gmail.com>
2024-04-01 22:04:46 +0100

    link glfw and open window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
-#include "adder.h"
+#include <adder.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <memory>
 
-int main(){
+int openWindow() {
+	GLFWwindow* window;
+	if (!glfwInit()) {
+		return -1;
+	}
+
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	return 0;
+}
+
+int main() {
 	float result = mymath::add(1.2f, 3.5f);
 	std::cout << result << std::endl;
-	return 0;
+
+	return openWindow();
 }
