a44c3795b9cdfc6ddd84606315f934fc42f872b9
tmanso9 <tmanso92@gmail.com>
2024-04-01 21:12:46 +0100

    library and test projects

---
+++ SomeLib/adder.h
@@ -0,0 +1,9 @@
+#ifndef ADDER_H
+# define ADDER_H
+
+namespace mymath {
+	int add(int a, int b);
+	float add(float a, float b);
+}
+
+#endif // ADDER_H
---
+++ TestSome/main.cpp
@@ -0,0 +1,8 @@
+#include <adder.h>
+#include <iostream>
+
+int main(){
+	float result = mymath::add(1.2f, 3.5f);
+	std::cout << result << std::endl;
+	return 0;
+}
